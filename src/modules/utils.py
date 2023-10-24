d3186b1f0cc12ce60f52454b38c703799463dec4
lilly-muyama <muyamalillian@gmail.com>
2023-10-24 10:47:08 +0200

    adding helper functions

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,59 +1,188 @@
-class Constants:
-    def init(self, args):
-        self.SEED = args.seed
-        self.CORRECT_DIAGNOSIS_REWARD = 1
-        self.INCORRECT_DIAGNOSIS_REWARD = -1
-        self.REPEATED_ACTION_REWARD = -1
-        self.MAX_LENGTH_REWARD = -1
-        self.FIRST_ACTION_REWARD = 0
 
-        self.ACTION_SPACE = ['No lupus', 'Lupus', 'Inconclusive diagnosis', 'ana', 'fever', 'leukopenia', 'thrombocytopenia', 'auto_immune_hemolysis', 'delirium',
-        'psychosis', 'seizure', 'non_scarring_alopecia', 'oral_ulcers', 'cutaneous_lupus', 'pleural_effusion', 'pericardial_effusion', 'acute_pericarditis',
-        'joint_involvement', 'proteinuria', 'biopsy_proven_lupus_nephritis', 'anti_cardioliphin_antibodies', 'anti_β2gp1_antibodies', 'lupus_anti_coagulant', 'low_c3', 
-        'low_c4', 'anti_dsdna_antibody', 'anti_smith_antibody']
+import ast
+import os
+import random
+import numpy as np
+import pandas as pd
+from modules.env import LupusEnv
+from modules.constants import constants
+import tensorflow as tf
+from stable_baselines import DQN
+from stable_baselines.common.callbacks import CheckpointCallback
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
-        self.ACTION_NUM = len(self.ACTION_SPACE)
-
-        self.CLASS_DICT = {'No lupus':0, 'Lupus':1, 'Inconclusive diagnosis':2} 
-
-        self.CLASS_NUM = len(self.CLASS_DICT)
-
-        self.FEATURE_NUM = self.ACTION_NUM - self.CLASS_NUM
-
-        self.MAX_STEPS = self.FEATURE_NUM + 1
-
-        self.CHECKPOINT_FREQ = 1000000
-
-        self.BETA = 9
+random.seed(constants.SEED)
+np.random.seed(constants.SEED)
+os.environ['PYTHONHASHSEED']=str(constants.SEED)
+tf.set_random_seed(constants.SEED)
+tf.compat.v1.set_random_seed(constants.SEED)
 
 
-        self.DOMAINS_FEAT_DICT = {'constitutional': ['fever'],
-        'hematologic': ['leukopenia', 'thrombocytopenia', 'auto_immune_hemolysis'],
-        'neuropsychiatric': ['delirium', 'psychosis', 'seizure'],
-        'mucocutaneous': ['non_scarring_alopecia', 'oral_ulcers', 'cutaneous_lupus'],
-        'serosal': ['pleural_effusion', 'pericardial_effusion', 'acute_pericarditis'],
-        'musculoskeletal': ['joint_involvement'],
-        'renal': ['proteinuria', 'biopsy_proven_lupus_nephritis'],
-        'antiphospholipid_antibodies': ['anti_cardioliphin_antibodies', 'anti_β2gp1_antibodies', 'lupus_anti_coagulant'],
-        'complement_proteins': ['low_c3', 'low_c4'],
-        'sle_specific_antibodies':['anti_dsdna_antibody', 'anti_smith_antibody']}
+def get_pathway_features_score(pathway):
+    if isinstance(pathway, str):
+        pathway = ast.literal_eval(pathway)
+    total_feature_cost = 0
+    for feat in pathway[:-1]:
+        feat_score = constants.FEATURE_SCORES[feat]
+        total_feature_cost += feat_score
+    return total_feature_cost
 
+def get_total_pathway_score(test_df):
+    total_score = 0
+    for i, row in test_df.iterrows():
+        row_feature_score = get_pathway_features_score(row.trajectory)
+        total_row_score = 1-(row_feature_score/constants.MAX_FEATURE_SCORE)
+        total_score += total_row_score
+        
+    return (total_score/len(test_df))*100
 
-        self.DOMAINS_MAX_SCORES_DICT = {'constitutional': 2, 'hematologic': 4, 'neuropsychiatric': 5, 'mucocutaneous': 6, 'serosal': 6, 'musculoskeletal': 6, 
-        'renal': 10, 'antiphospholipid_antibodies': 2, 'complement_proteins': 4, 'sle_specific_antibodies':6}
+def get_weighted_pahm_score(numbers, weights): #both are list of the same length
+    whm = (weights[0] + weights[1]) / ((weights[0] / numbers[0]) + (weights[1] / numbers[1]))
+    return whm
 
-        self.CRITERIA_WEIGHTS = {'fever':2, 'leukopenia':3, 'thrombocytopenia':4, 'auto_immune_hemolysis':4, 'delirium':2, 'psychosis':3, 'seizure':5, 
-        'non_scarring_alopecia':2, 'oral_ulcers':2, 'subacute_cutaneous_lupus':4, 'discoid_lupus':4, 'acute_cutaneous_lupus':6, 'pleural_effusion': 5, 
-        'pericardial_effusion':5, 'acute_pericarditis':6, 'joint_involvement':6, 'proteinuria':4, 'renal_biopsy_1':0, 'renal_biopsy_2':8, 'renal_biopsy_3':10, 
-        'renal_biopsy_4':10, 'renal_biopsy_5':8, 'anti_cardioliphin_antibodies':2, 'anti_β2gp1_antibodies':2, 'lupus_anti_coagulant':2, 'low_c3':3, 'low_c4':3, 
-        'low_c3_and_low_c4':4, 'low_c3_or_low_c4':3, 'anti_dsdna_antibody':6, 'anti_smith_antibody':6}
+def load_dqn(filename, env=None):
+    '''
+    Loads a previously saved DQN model
+    '''
+    model = DQN.load(filename, env=env)
+    return model
 
-        self.FEATURE_SCORES = {'ana':11.5, 'fever':15, 'leukopenia':12, 'thrombocytopenia':12, 'auto_immune_hemolysis':12, 'delirium':15, 'psychosis':15, 'seizure':15, 
-        'non_scarring_alopecia':15, 'oral_ulcers':15, 'cutaneous_lupus':6.5, 'pleural_effusion':13, 'pericardial_effusion':13, 'acute_pericarditis':13, 
-        'joint_involvement':13, 'proteinuria':13.5, 'biopsy_proven_lupus_nephritis':1, 'anti_cardioliphin_antibodies':10, 'anti_β2gp1_antibodies':9.5, 
-        'lupus_anti_coagulant':9.5, 'low_c3':10.5, 'low_c4':10.5, 'anti_dsdna_antibody':11.5, 'anti_smith_antibody':11.5}
+def create_env(X, y, random=True):
+    '''
+    Creates and environment using the given data
+    '''
+    env = LupusEnv(X, y, random)
+    print(f'The environment seed is {env.seed()}') #to delete
+    return env
 
-        self.MAX_FEATURE_SCORE = 283.5
-        self.REG_FEATURE_SCORE = 0.9
+def stable_vanilla_dqn(X_train, y_train, timesteps, save=False, log_path=None, log_prefix='dqn', filename=None, per=False):
+    '''
+    Creates and trains a standard DQN model
+    '''
+    if per:
+        log_prefix = 'dqn_per'
+    training_env = create_env(X_train, y_train)
+    model = DQN('MlpPolicy', training_env, verbose=1, seed=constants.SEED, learning_rate=0.0001, buffer_size=1000000, learning_starts=50000, 
+                train_freq=4, target_network_update_freq=10000, exploration_final_eps=0.05, n_cpu_tf_sess=1, policy_kwargs=dict(dueling=False),
+                double_q=False, prioritized_replay=per)
+    
+    checkpoint_callback = CheckpointCallback(save_freq=constants.CHECKPOINT_FREQ, save_path=log_path, name_prefix=log_prefix)
+    model.learn(total_timesteps=timesteps, log_interval=100000, callback=checkpoint_callback)
+    if save:
+        model.save(f'{log_path}/{filename}.pkl')
+    training_env.close()
+    return model
 
-constants = Constants()
+def stable_dueling_dqn(X_train, y_train, timesteps, save=False, log_path=None, log_prefix='dueling_dqn', filename=None, per=False):
+    '''
+    Creates and trains a dueling DQN model
+    '''
+    print(F'Using seed {constants.SEED}')
+    if per:
+        log_prefix = 'dueling_dqn_per'
+    training_env = create_env(X_train, y_train)
+    model = DQN('MlpPolicy', training_env, verbose=1, seed=constants.SEED, learning_rate=0.0001, buffer_size=1000000, learning_starts=50000, 
+                train_freq=4, target_network_update_freq=10000, exploration_final_eps=0.05, n_cpu_tf_sess=1, double_q=False, prioritized_replay=per)
+    
+    checkpoint_callback = CheckpointCallback(save_freq=constants.CHECKPOINT_FREQ, save_path=log_path, name_prefix=log_prefix)
+    model.learn(total_timesteps=timesteps, log_interval=100000, callback=checkpoint_callback)
+    if save:
+        model.save(f'{log_path}/{filename}.pkl')
+    training_env.close()
+    return model
+
+def stable_dueling_ddqn(X_train, y_train, timesteps, save=False, log_path=None, log_prefix='dueling_ddqn', filename=None, per=False):
+    '''
+    Creates and trains a dueling double DQN model
+    '''
+    if per:
+        log_prefix = 'dueling_ddqn_per'
+    training_env = create_env(X_train, y_train)
+    model = DQN('MlpPolicy', training_env, verbose=1, seed=constants.SEED, learning_rate=0.0001, buffer_size=1000000, learning_starts=50000, 
+                train_freq=4, target_network_update_freq=10000, exploration_final_eps=0.05, n_cpu_tf_sess=1, prioritized_replay=per)
+    
+    checkpoint_callback = CheckpointCallback(save_freq=constants.CHECKPOINT_FREQ, save_path=log_path, name_prefix=log_prefix)
+    model.learn(total_timesteps=timesteps, log_interval=100000, callback=checkpoint_callback)
+    if save:
+        model.save(f'{log_path}/{filename}.pkl')
+    training_env.close()
+    return model
+
+def stable_double_dqn(X_train, y_train, timesteps, save=False, log_path=None, log_prefix ='ddqn', filename=None, per=False):
+    '''
+    Creates and trains a double DQN model
+    '''
+    if per:
+        log_prefix = 'ddqn_per'
+    training_env = create_env(X_train, y_train)
+    model = DQN('MlpPolicy', training_env, verbose=1, seed=constants.SEED, learning_rate=0.0001, buffer_size=1000000, learning_starts=50000, 
+                train_freq=4, target_network_update_freq=10000, exploration_final_eps=0.05, n_cpu_tf_sess=1, policy_kwargs=dict(dueling=False), 
+                prioritized_replay=per)
+    
+    checkpoint_callback = CheckpointCallback(save_freq=constants.CHECKPOINT_FREQ, save_path=log_path, name_prefix=log_prefix)
+    model.learn(total_timesteps=timesteps, log_interval=100000, callback=checkpoint_callback)
+    if save:
+        model.save(f'{log_path}/{filename}.pkl')
+    training_env.close()
+    return model
+
+def multiclass(actual_class, pred_class, average = 'macro'):
+    '''
+    Returns the ROC-AUC score for multi-labeled data
+    '''
+
+    unique_class = set(actual_class)
+    roc_auc_dict = {}
+    for per_class in unique_class:
+        other_class = [x for x in unique_class if x != per_class]
+        new_actual_class = [0 if x in other_class else 1 for x in actual_class]
+        new_pred_class = [0 if x in other_class else 1 for x in pred_class]
+        roc_auc = roc_auc_score(new_actual_class, new_pred_class, average = average)
+        roc_auc_dict[per_class] = roc_auc
+    avg = sum(roc_auc_dict.values()) / len(roc_auc_dict)
+    return avg
+
+def test(ytest, ypred):
+    '''
+    Return performance metrics for a model
+    '''
+    acc = accuracy_score(ytest, ypred)
+    f1 = f1_score(ytest, ypred, average ='macro', labels=np.unique(ytest))
+    try:
+        roc_auc = multiclass(ytest, ypred)
+    except:
+        roc_auc = None
+    return acc*100, f1*100, roc_auc*100
+
+def evaluate_dqn(dqn_model, X_test, y_test):
+    '''
+    Evaluates a DQN model on test data
+    '''
+    test_df = pd.DataFrame()
+    env = create_env(X_test, y_test, random=False)
+    count=0
+
+    try:
+        while True:
+            count+=1
+            obs, done = env.reset(), False
+            while not done:
+                action, _states = dqn_model.predict(obs, deterministic=True)
+                obs, rew, done, info = env.step(action)
+                if done == True:
+                    test_df = test_df.append(info, ignore_index=True)
+    except StopIteration:
+        pass
+    return test_df
+
+def get_val_metrics(model, X_val, y_val):
+    val_df = evaluate_dqn(model, X_val, y_val)
+    acc, f1, roc_auc, = test(val_df['y_actual'], val_df['y_pred'])
+    pathway_score = get_total_pathway_score(val_df)
+    wpahm_score = get_weighted_pahm_score([acc, pathway_score], [0.9, 0.1])
+    min_path_length = val_df.episode_length.min()
+    average_path_length = val_df.episode_length.mean()
+    max_path_length = val_df.episode_length.max()
+    min_sample_pathway = val_df[val_df.episode_length==min_path_length].trajectory.iloc[0]
+    max_sample_pathway = val_df[val_df.episode_length==max_path_length].trajectory.iloc[0]
+    return pathway_score, wpahm_score, acc, f1, roc_auc, min_path_length, average_path_length, max_path_length, min_sample_pathway, max_sample_pathway
